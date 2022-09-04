81a85d750059ce0330c117a5b2afa26db7b0d03e
sidratul <muntaher1025@gmail.com>
2022-09-04 10:19:22 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapp/Night_light.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Night_light extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night_light);
+    }
+}
